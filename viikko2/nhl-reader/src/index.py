59c1e7289d53cd244e79ceff3e09e52ab3908f91
Pinja Paivakumpu <paivakumpupinja@gmail.com>
2025-11-09 11:05:26 +0200

    2: riippuvuuksien hyödyntäminen

---
+++ viikko2/nhl-reader/src/index.py
@@ -0,0 +1,22 @@
+import requests
+from player import Player
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    print("Players from FIN:")
+
+    for player in players:
+        if player.nationality == 'FIN':
+
+            print(player)
+
+if __name__ == "__main__":
+    main()
