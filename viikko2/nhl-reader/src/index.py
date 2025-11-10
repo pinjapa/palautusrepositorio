947e3012ed7fbdd9708fc17961f9ac4b27a5d837
Pinja Paivakumpu <paivakumpupinja@gmail.com>
2025-11-10 14:25:13 +0200

    5: graafinen pelaajalista

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,5 +1,8 @@
 import requests
 from player import Player
+from rich.console import Console
+from rich.table import Table
+from rich.prompt import Prompt
 
 class PlayerReader:
     def __init__(self, url):
@@ -32,14 +35,33 @@ class PlayerStats:
         return result
 
 def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
-    reader = PlayerReader(url)
-    stats = PlayerStats(reader)
-    players = stats.top_scorers_by_nationality("FIN")
-    
-    for player in players:
-        print(player)
-    
+    seasons = ["2018-19","2019-20","2020-21","2021-22","2022-23","2023-24","2024-25"]
+    nationalities = ["USA","FIN","CAN","SWE","CZE","RUS","SLO","FRA","GBR","SVK",
+                     "DEN","NED","AUT","BLR","GER","SUI","NOR","UZB", "LAT", "AUS"]
+    season = Prompt.ask("Season", choices=seasons)
+    while True:
+        nationality = Prompt.ask("Nationality", choices=nationalities)    
+        url = f"https://studies.cs.helsinki.fi/nhlstats/{season}/players"
+        reader = PlayerReader(url)
+        stats = PlayerStats(reader)
+        players = stats.top_scorers_by_nationality(nationality)
+
+        table = Table(title=f"Season {season} players from {nationality}")
+
+        table.add_column("Player", justify="right", style="cyan", no_wrap=True)
+        table.add_column("Teams", style="magenta")
+        table.add_column("Goals", justify="right", style="green")
+        table.add_column("Assists", justify="right", style="green")
+        table.add_column("Points", justify="right", style="green")
+
+        for player in players:
+            table.add_row(
+                str(player.name), str(player.team),
+                  str(player.goals), str(player.assists), str(player.points))
+        
+
+        console = Console()
+        console.print(table)
     
 
 if __name__ == "__main__":
